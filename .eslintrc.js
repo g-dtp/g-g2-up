b726ca58cc0935e709db7254f62647c28b4aef6a
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-04-29 14:11:22 +0800

    wip element 按需引用

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,9 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-tabs': 0,
     'no-mixed-spaces-and-tabs': 0,
-    'indent': ["off", "tab"],
+    'indent': ['off', 'tab'],
     'no-trailing-spaces': 1,
-    'space-before-function-paren': 0 //这句话表示在函数后可以不加空格
+    'space-before-function-paren': 0 // 这句话表示在函数后可以不加空格
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import G2Up from '../lib/g2-up.umd'
+import './plugins/element.js'
 console.log(G2Up)
 Vue.config.productionTip = false
 
