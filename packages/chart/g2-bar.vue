7c402bdcf6ee27bc02530e36688fb4a10bb6ac4b
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-05-23 13:23:50 +0800

    feat: 柱状加label

--- packages/chart/g2-bar.vue
+++ packages/chart/g2-bar.vue
@@ -22,19 +22,28 @@
 		watch: {},
 		methods: {
 			drawChart () {
+				let labelConfig = {
+					position: 'middle',
+					offset: 0,
+					textStyle: {
+						fontSize: 11,
+						fill: '#FFF'
+					}
+				}
+
 				this.chart && this.chart.clear()
 				this.dv = ds.createView().source(this.chartData)
 				this.dv.transform(this.getTransformMapNull())
 				if (this.measure.length > 1) {
 					this.dv.transform(this.getTransformFold())
 					this.chart.source(this.dv)
-					this.chart.intervalStack().position(`${this.dimension}*value`).color('type')
+					this.chart.intervalStack().position(`${this.dimension}*value`).color('type').label('value', labelConfig)
 				} else {
 					this.chart.source(this.dv)
 					if (this.legend) {
-						this.chart.intervalStack().position(`${this.dimension}*${this.measure[0]}`).color(this.legend)
+						this.chart.intervalStack().position(`${this.dimension}*${this.measure[0]}`).color(this.legend).label(this.measure[0], labelConfig)
 					} else {
-						this.chart.interval().position(`${this.dimension}*${this.measure[0]}`)
+						this.chart.interval().position(`${this.dimension}*${this.measure[0]}`).label(this.measure[0], labelConfig)
 					}
 				}
 				this.chart.render()
