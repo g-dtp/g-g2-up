d90dd51c901d0de3f443bf3ac8de3284c9a11269
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-06-10 17:45:58 +0800

    feat: 优化环形图

--- packages/chart/g2-ring.vue
+++ packages/chart/g2-ring.vue
@@ -5,7 +5,7 @@
 <script>
 	import { DataSet } from '@antv/data-set'
 	import G2Serie from './base/g2-serie'
-
+	import { toFixed2 } from './formatter'
 	const ds = new DataSet()
 	export default {
 		extends: G2Serie,
@@ -24,18 +24,58 @@
 			drawChart () {
 				this.chart && this.chart.clear()
 				this.chart.coord('theta', { innerRadius: 0.75 })
+				this.chart.tooltip({
+					showTitle: false
+				})
+				this.chart.legend(false)
 				this.dv = ds.createView()
 					.source(this.chartData)
 					.transform(this.getTransformMapNull())
-					.transform({
-						type: 'fold',
-						fields: this.measure,
-						dimension: this.dimension,
-						key: 'type',
-						value: 'value'
+				if (this.dimension && this.measure.length === 0) {
+					this.dv.transform({
+						type: 'map',
+						callback(row) {
+							row.count = 1
+							return row
+						}
 					})
-				this.chart.source(this.dv)
-				this.chart.intervalStack().position('value').color(this.dimension)
+					this.chart.source(this.dv)
+					this.chart.intervalStack().position('count').color(this.dimension)
+				} else {
+					this.dv.transform({
+							type: 'fold',
+							fields: this.measure,
+							dimension: this.dimension,
+							key: 'type',
+							value: 'value'
+						})
+					this.dv.transform({
+						type: 'aggregate',
+						fields: 'value',
+						operations: ['sum'],
+						as: ['total'],
+						groupBy: [this.dimension]
+					})
+					this.dv.transform({
+						type: 'percent',
+						field: 'total',
+						dimension: this.dimension,
+						as: 'percent'
+					})
+					this.chart.source(this.dv)
+					this.chart.intervalStack()
+						.position('percent')
+						.color(this.dimension)
+						.label(this.dimension + '*percent', (label, percent) => {
+							return label + '' + toFixed2(percent)
+						})
+						.tooltip(this.dimension + '*percent', function(item, percent) {
+							return {
+								name: item,
+								value: toFixed2(percent)
+							}
+						})
+				}
 				this.chart.render()
 			}
 		}
