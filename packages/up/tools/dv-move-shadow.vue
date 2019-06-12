92e01e1a24675459ba33fbe2eaeeebff7169d0b0
jiangxiaosong <xiaosong402@sina.com>
2019-06-12 21:54:11 +0800

    wip: 仿照PowerBi磁铁

---
+++ packages/up/tools/dv-move-shadow.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+	.dv-move-shadow(:style="style")
+</template>
+
+<script>
+	export default {
+		name: 'dv-move-shadow',
+		props: {
+			w: {
+				default: 240
+			},
+			h: {
+				default: 180
+			},
+			position: {
+				type: Object,
+				default: function () {
+					return {
+						x: 0,
+						y: 0
+					}
+				}
+			}
+		},
+		computed: {
+			style() {
+				return {
+					width: this.w + 'px',
+					height: this.h + 'px',
+					left: this.position.x + 'px',
+					top: this.position.y + 'px'
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.dv-move-shadow {
+		position: absolute;
+		transition: transform .25s, width .25s, height .25s, left  .25s , top .25s ;
+		background: rgba(0, 0, 0, 0.4);
+	}
+</style>
--- src/views/move.vue
+++ src/views/move.vue
@@ -1,14 +1,31 @@
 <template lang="pug">
 	.move
 		.move-warp
-			dv-move
+			dv-move-shadow(v-if='dragging' :position="position")
+			dv-move(@dragging="onDragging" @drag-end="onDragEnd")
 </template>
 
 <script>
 	import DvMove from '../../packages/up/dv-move'
+	import DvMoveShadow from '../../packages/up/tools/dv-move-shadow'
 	export default {
 		name: 'move',
-		components: { DvMove }
+		components: { DvMove, DvMoveShadow },
+		data() {
+			return {
+				dragging: false,
+				position: ''
+			}
+		},
+		methods: {
+			onDragging(position) {
+				this.dragging = true
+				this.position = position
+			},
+			onDragEnd() {
+				this.dragging = false
+			}
+		}
 	}
 </script>
 
