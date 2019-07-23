beaefb39205fadd951f247c6ac0f7bb8dda2a03a
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-07-23 17:35:48 +0800

    wip: 中国地图

--- packages/chart/map/g2-china-map.vue
+++ packages/chart/map/g2-china-map.vue
@@ -26,13 +26,14 @@
 				this.geoDv = ds.createView().source(ChinaData, {
 					type: 'GeoJSON'
 				})
-				// this.dv = ds.createView().source(userData).transform({
-				// 	geoDataView: this.geoDv,
-				// 	field: 'name',
-				// 	type: 'geo.centroid',
-				// 	as: ['longitude', 'latitude']
-				// })
-				console.log(/dv/, this.geoDv)
+				let dimension = this.dimension[0]
+				this.dv = ds.createView().source(this.chartData).transform({
+					geoDataView: this.geoDv,
+					field: dimension,
+					type: 'geo.region',
+					as: ['longitude', 'latitude']
+				})
+				console.log(this.dv)
 				this.chart && this.chart.clear()
 				this.chart.scale({
 					longitude: {
@@ -54,16 +55,32 @@
 				let bgView = this.chart.view()
 				bgView.source(this.geoDv)
 				bgView.tooltip(false)
-				bgView.polygon().position('longitude*latitude').color('#ebedf0').style({
-					lineWidth: 1,
-					stroke: '#fafbfc'
-				}).active(false)
-				// let userView = this.chart.view()
-				// userView.source(this.dv)
-				// userView.point().position('longitude*latitude').color('#1890ff').opacity(0.6).size('value', [5, 15]).style({
-				// 	lineWidth: 1,
-				// 	stroke: '#1890ff'
-				// }).shape('circle')
+				bgView.polygon()
+					.position('longitude*latitude')
+					.label(dimension, {
+						textStyle: {
+							fill: '#fff',
+							fontSize: 10,
+							shadowBlur: 2,
+							shadowColor: 'rgba(0, 0, 0, .45)'
+						}
+					})
+					.color('#ebedf0')
+					.style({
+						lineWidth: 1,
+						stroke: '#fafbfc'
+					})
+					.active(false)
+				let userView = this.chart.view()
+				userView.source(this.dv)
+				userView.polygon()
+					.position('longitude*latitude')
+
+					.style({
+						stroke: '#fff',
+						lineWidth: 1
+					})
+					.color('人口', '#BAE7FF-#1890FF-#0050B3')
 				this.chart.render()
 			}
 		}
