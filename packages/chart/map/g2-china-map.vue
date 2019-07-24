de7d49a64d229e7a5c2f467c3a509688127bf6b1
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-07-24 16:06:36 +0800

    优化

--- packages/chart/map/g2-china-map.vue
+++ packages/chart/map/g2-china-map.vue
@@ -19,7 +19,12 @@
 			}
 		},
 		mounted() {
-			this.drawChart()
+			try {
+				this.drawChart()
+			} catch (e) {
+
+			}
+
 		},
 		methods: {
 			drawChart() {
@@ -29,82 +34,50 @@
 				this.chartData.forEach(d => {
 					mapChartData[d[dimension]] = d[measure]
 				})
-				console.log(/mapChartData/, mapChartData)
-				this.geoDv = ds.createView().source(ChinaData, {
-					type: 'GeoJSON'
-				})
-				this.geoDv.transform({
-					type: 'map',
-					callback(row) { // 加工数据后返回新的一行，默认返回行数据本身
-						row[measure] = mapChartData[row.name] || 0
-						console.log(row)
-						return row
-					}
-				})
-				this.dv = ds.createView().source(this.chartData)
-					.transform({
-						geoDataView: this.geoDv,
-						field: dimension,
-						type: 'geo.region',
-						as: ['longitude', 'latitude']
+				this.dv = ds.createView()
+					.source(ChinaData, {
+						type: 'GeoJSON'
 					})
-
+					.transform({
+						type: 'map',
+						callback(row) {
+							row[measure] = mapChartData[row.name] || 0
+							return row
+						}
+					})
 				this.chart && this.chart.clear()
-				this.chart.scale({
-					longitude: {
-						sync: true
-					},
-					latitude: {
-						sync: true
-					}
-				})
 				this.chart.axis(false)
 				this.chart.legend({
 					name: false,
 					reactive: true,
+					slidable: false,
 					sizeType: 'circle'
 				})
 				this.chart.tooltip(true, {
-					showTitle: false
+					showTitle: true,
+					title: dimension
 				})
-				// let bgView = this.chart.view()
-				// bgView.source(this.geoDv)
-				// bgView.tooltip(false)
-				// bgView.polygon()
-				// 	.position('longitude*latitude')
-				// 	.label(dimension, {
-				// 		textStyle: {
-				// 			fill: '#000',
-				// 			fontSize: 12,
-				// 			shadowColor: 'rgba(0, 0, 0, .45)'
-				// 		}
-				// 	})
-				// 	.color('#ebedf0')
-				// 	.style({
-				// 		lineWidth: 1,
-				// 		stroke: '#fafbfc'
-				// 	})
-				// 	.active(false)
-				let userView = this.chart.view()
-				userView.source(this.geoDv)
-				userView.tooltip(`${dimension}*${measure}`, (a, b) => {
+				this.chart.source(this.dv)
+				this.chart.tooltip(`${dimension}*${measure}`, (a, b) => {
 					return {
 						name: a,
 						value: b
 					}
 				})
-				userView.polygon()
+				this.chart.polygon()
 					.position('longitude*latitude')
 					.style({
 						stroke: '#fff',
-						lineWidth: 1
+						lineWidth: 2
+					})
+					.label(dimension, {
+						textStyle: {
+							fill: '#000',
+							fontSize: 10
+						}
 					})
 					.color(measure, '#ebedf0-#0050B3')
-				try {
-					this.chart.render()
-				} catch (e) {
-
-				}
+				this.chart.render()
 			}
 		}
 	}
